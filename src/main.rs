a80f64f10585d78bee110c5beec4000430249181
dseum <dseum.me@gmail.com>
2025-04-10 14:19:58 -0400

    temporary save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io;
+use std::path::Path;
+
+mod migrations;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!(
+            "Usage: {} <migrations_dir_path> <statements_dir_path>",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let migrations_dir_path = &args[1];
+    let migrations_dir = Path::new(migrations_dir_path);
+
+    let statements_dir_path = &args[2];
+    let statements_dir = Path::new(statements_dir_path);
+
+    let _ = migrations::process(migrations_dir);
+    Ok(())
 }
